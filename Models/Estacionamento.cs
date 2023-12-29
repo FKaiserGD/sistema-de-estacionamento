34c62fb0829a18a4914a0c2c9fbf7f034c466944
Filipe Lacerda <101232217+FKaiserGD@users.noreply.github.com>
2023-12-29 01:11:50 -0300

    Version 1.0

---
+++ Models/Estacionamento.cs
@@ -0,0 +1,81 @@
+// Esta classe representa o modelo do sistema de estacionamento.
+// Contém métodos para adicionar, remover e listar veículos, bem como informações sobre os preços.
+namespace Models
+{
+    public class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        // Construtor que recebe os preços iniciais e por hora.
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+        }
+
+        // Método para adicionar um veículo ao estacionamento.
+        public void AdicionarVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string placa = Console.ReadLine().ToUpper();
+
+            if (placa.Length == 7 && placa.All(char.IsLetterOrDigit))
+            {
+                veiculos.Add(placa);
+                Console.WriteLine($"Veículo com placa {placa} estacionado com sucesso.");
+            }
+            else
+            {
+                Console.WriteLine("Placa inválida. A placa deve conter exatamente 7 caracteres alfanuméricos.");
+            }
+        }
+
+        // Método para remover um veículo do estacionamento.
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veículo para remover:");
+
+            string placa = Console.ReadLine();
+
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                if (int.TryParse(Console.ReadLine(), out int horas))
+                {
+                    decimal valorTotal = precoInicial + precoPorHora * horas;
+
+                    veiculos.Remove(placa);
+
+                    Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal:F2}");
+                }
+                else
+                {
+                    Console.WriteLine("Valor de horas inválido.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+            }
+        }
+
+        // Método para listar os veículos estacionados.
+        public void ListarVeiculos()
+        {
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veículos estacionados são:");
+                foreach (var veiculo in veiculos)
+                {
+                    Console.WriteLine(veiculo);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using Models;
+// Este código representa a interação principal com o sistema de estacionamento.
+// Inicia-se com a configuração da codificação para permitir a exibição de acentos.
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+// Declaração e inicialização das variáveis de preço inicial e por hora.
+decimal precoInicial = 0;
+decimal precoPorHora = 0;
+// Solicita ao usuário inserir o preço inicial e por hora do estacionamento.
+Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
+                  "Digite o preço inicial:");
+precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Agora digite o preço por hora:");
+precoPorHora = Convert.ToDecimal(Console.ReadLine());
+// Instancia a classe Estacionamento com os valores obtidos anteriormente.
+Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+
+string opcao = string.Empty;
+bool exibirMenu = true;
+// Loop do menu principal, exibindo opções ao usuário.
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção:");
+    Console.WriteLine("1 - Cadastrar veículo");
+    Console.WriteLine("2 - Remover veículo");
+    Console.WriteLine("3 - Listar veículos");
+    Console.WriteLine("4 - Encerrar");
+    // Switch statement para processar a escolha do usuário.
+    switch (Console.ReadLine())
+    {
+        case "1":
+            es.AdicionarVeiculo();
+            break;
+
+        case "2":
+            es.RemoverVeiculo();
+            break;
+
+        case "3":
+            es.ListarVeiculos();
+            break;
+
+        case "4":
+            exibirMenu = false;
+            Console.WriteLine("O programa se encerrou");
+            Environment.Exit(0);
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida. Por favor, escolha uma opção válida.");
+            break;
+    }
+
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
+}
+
+Console.WriteLine("O programa se encerrou");
